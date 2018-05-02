92b6ee2dd09b1d39a31808f0f113802c5c091c28
Farshad Mousalou <farshadm90@gmail.com>
2018-05-02 09:16:42 +0430

    - added View To Project and implement - added UIView+autolayout extension

---
+++ SevenWestMediaExample/Classes/View/ServiceDataViewController.swift
@@ -0,0 +1,223 @@
+//
+//  ServiceDataViewController.swift
+//  SevenWestMediaExample
+//
+//  Created by Farshad Mousalou on 5/2/18.
+//  Copyright © 2018 Farshad Mousalou. All rights reserved.
+//
+
+import UIKit
+
+private let cellId = "cellId"
+
+class ServiceDataViewController: UITableViewController {
+    
+    var activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+    
+    var viewModal : ServerDataViewModel? = ServerDataViewModel() {
+        didSet{
+            bindViewModal()
+        }
+    }
+    
+    var disposal : Disposal = []
+    
+    deinit {
+        disposal.removeAll()
+    }
+    
+    private var refreshControll : UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if #available(iOS 11.0, *) {
+            self.navigationItem.largeTitleDisplayMode = .automatic
+             self.navigationController?.navigationBar.prefersLargeTitles = true
+        }
+       
+        
+        setupTableView()
+        setupLoadingView()
+        
+        
+        if viewModal == nil {
+            viewModal = .init()
+        }
+        
+        bindViewModal()
+        loadData()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    //MARK: - ViewModel Methods
+    
+    func bindViewModal(){
+        
+        guard let viewModal = viewModal else {
+            return
+        }
+        
+        viewModal.isLoading.observe({[unowned self] in self.handleLoadingView($0); _ = $1 }).add(to: &disposal)
+        viewModal.title.observe({[unowned self] in self.title = $0; _ = $1 }).add(to: &disposal)
+        viewModal.rows.observe({[unowned self] (_,_) in self.reloadData() }).add(to: &disposal)
+        
+    }
+    
+    /// <#Description#>
+    func loadData() {
+        
+        viewModal?.loadData {[weak self] (result, error) in
+            
+            guard let s = self else { return }
+            
+            guard let error = error else {
+                return
+            }
+            
+            s.handleFetch(error:error) { retry in
+                
+                if retry {
+                    s.loadData()
+                }
+                
+            }
+        }
+    }
+    
+    /// <#Description#>
+    func reloadData(){
+        self.tableView.reloadData()
+    }
+    
+    @objc
+    func refreshData(_ sender : UIRefreshControl? = nil ){
+        
+        self.loadData()
+        
+    }
+    
+    //MARK: - Loading View Methods
+    
+    /// <#Description#>
+    fileprivate func setupLoadingView() {
+        
+        self.view.addSubview(activityIndicatorView)
+        
+        self.view.bringSubview(toFront: activityIndicatorView)
+        
+        activityIndicatorView.hidesWhenStopped = true
+        
+        activityIndicatorView.alignToSuperviewAxis(.vertical).isActive = true
+        activityIndicatorView.alignToSuperviewAxis(.horizontal).isActive = true
+        activityIndicatorView.set(.width, to: 50.0)
+        activityIndicatorView.set(.height, to: 50.0)
+        
+    }
+    
+    /// <#Description#>
+    ///
+    /// - Parameter isLoading: <#isLoading description#>
+    func handleLoadingView(_ isLoading : Bool ) {
+       
+        guard viewModal?.rows.value.count == 0 else {
+            
+            refreshControll.endRefreshing()
+            
+            return
+        }
+        
+        if isLoading {
+            
+            self.activityIndicatorView.startAnimating()
+        }else {
+            self.activityIndicatorView.stopAnimating()
+        }
+        
+//        self.tableView.isHidden = isLoading
+        
+    }
+    
+    //MARK: - Error Handler
+    
+    /// <#Description#>
+    ///
+    /// - Parameters:
+    ///   - error: <#error description#>
+    ///   - completion: <#completion description#>
+    func handleFetch(error:Error,completion:@escaping (Bool) -> () ){
+        
+        let alertView = UIAlertController(title: NSLocalizedString("Error", comment: ""),
+                                         message: error.localizedDescription, preferredStyle: .alert)
+        
+        alertView.addAction(UIAlertAction(title: NSLocalizedString("cancel", comment: ""), style: .cancel, handler: { (cancelAction) in
+            completion(false)
+        }))
+        
+        alertView.addAction(UIAlertAction(title: NSLocalizedString("retry ", comment: ""), style: .default, handler: { (retryAction) in
+            completion(true)
+        }))
+        
+        self.present(alertView, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - tableView
+    private func setupTableView() {
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.estimatedRowHeight = 85.0
+        self.tableView.register(ServiceRowViewCell.self, forCellReuseIdentifier:cellId)
+        
+        refreshControll = UIRefreshControl()
+        refreshControll.addTarget(self, action: #selector(refreshData(_:)), for: UIControlEvents.valueChanged)
+        refreshControll.tintColor = UIColor.blue
+        
+        if #available(iOS 10, *){
+            self.tableView.refreshControl = refreshControll
+        }else{
+            self.tableView.addSubview(refreshControll)
+        }
+        
+        self.tableView.tableFooterView = UIView(frame:.zero)
+    }
+    
+    // MARK: - TableViewController  Override Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModal?.rows.value.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:cellId, for: indexPath) as! ServiceRowViewCell
+        
+        guard let rowViewModel = viewModal?[at:indexPath.row] else {
+            return cell
+        }
+        
+        cell.config(viewModal:rowViewModel)
+        
+        cell.selectionStyle = .none
+        
+        return cell
+        
+    }
+    
+}
---
+++ SevenWestMediaExample/Classes/View/ServiceRowViewCell.swift
@@ -0,0 +1,153 @@
+//
+//  ServiceRowViewCell.swift
+//  SevenWestMediaExample
+//
+//  Created by Farshad Mousalou on 5/2/18.
+//  Copyright © 2018 Farshad Mousalou. All rights reserved.
+//
+
+import UIKit
+
+class ServiceRowViewCell: UITableViewCell {
+
+    var titleLabel = UILabel()
+    var descriptionLabel = UILabel()
+    var rowImageView = UIImageView()
+    
+    var disposal : Disposal = []
+    
+    deinit {
+        disposal.removeAll()
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    override func prepareForReuse() {
+        disposal.removeAll()
+        
+        self.titleLabel.text = nil
+        self.descriptionLabel.text = nil
+        self.rowImageView.image = nil
+        
+        super.prepareForReuse()
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    func commonInit(){
+        let fakeView = UIView()
+        self.contentView.addSubview(fakeView)
+        fakeView.backgroundColor = nil
+        fakeView.pinToSuperviewEdges()
+        fakeView.heightAnchor.constraint(greaterThanOrEqualToConstant: 85.0)
+        
+        
+        // Added Subview
+        self.contentView.addSubview(self.rowImageView)
+        self.contentView.addSubview(self.descriptionLabel)
+        self.contentView.addSubview(self.titleLabel)
+        
+        self.rowImageView.contentMode = .scaleAspectFit
+        self.rowImageView.backgroundColor = .gray
+        
+        self.titleLabel.numberOfLines = 3
+        self.titleLabel.textColor = .black
+        self.titleLabel.font = UIFont.systemFont(ofSize: 20.0, weight: .medium)
+        
+        self.descriptionLabel.numberOfLines = 0
+        self.descriptionLabel.font = UIFont.systemFont(ofSize: 17.0, weight: .regular)
+        
+        
+        // set autolayout for rowImageView
+        self.rowImageView.pinToSuperviewEdge(.leading, inset: 16, priority:.required).isActive = true
+        self.rowImageView.set(.height, to: 50)
+        
+        self.rowImageView.topAnchor.constraint(greaterThanOrEqualTo: self.contentView.topAnchor, constant: 16).isActive = true
+        let imageBottomCont = self.rowImageView.bottomAnchor.constraint(greaterThanOrEqualTo: self.contentView.bottomAnchor, constant: 16)
+        imageBottomCont.priority = UILayoutPriority(rawValue: 850)
+        imageBottomCont.isActive = true
+        
+        self.rowImageView.alignToSuperviewAxis(.vertical)
+        self.rowImageView.widthAnchor.constraint(equalTo: self.rowImageView.heightAnchor, multiplier: 1/1, constant: 0).isActive = true
+        
+        
+        // create autolayout for titleLabel with content compression resistance required
+        self.titleLabel.pinToSuperviewEdge(.top, inset: 16, priority: .required).isActive = true
+        self.titleLabel.pinToSuperviewEdge(.trailing, inset: 16, priority: .required).isActive = true
+        self.titleLabel.setContentCompressionResistance(for: .vertical, to: .required)
+        
+        // pin titleLabel leading edge to trailing of rowImageView with 8 inset
+        self.titleLabel.pinEdge(.leading, to:.trailing, of: self.rowImageView, inset: 16, priority: .required).isActive = true
+        // pin titleLabel bottom edge to top of descriptionLabel with 8 inset
+        self.titleLabel.pinEdge(.bottom, to: .top, of: self.descriptionLabel, inset: 16, priority: .required).isActive = true
+        
+        // create autolayout for descriptionLabel with content compression resistance required
+        self.descriptionLabel.pinToSuperviewEdge(.trailing, inset: 16, priority: .required).isActive = true
+        self.descriptionLabel.pinToSuperviewEdge(.bottom, inset: 16, priority: .required).isActive = true
+        self.descriptionLabel.pinEdge(.leading, to: .leading, of: self.titleLabel, inset: 0, priority: .required).isActive = true
+        self.descriptionLabel.setContentCompressionResistance(for: .vertical, to: .required)
+        self.descriptionLabel.setContentHuggingPriority(.required, for: .vertical)
+        
+        if #available(iOS 11.0, *) {
+            self.rowImageView.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        } else {
+            // Fallback on earlier versions
+        }
+
+        
+    }
+    
+    func config(viewModal : ServerRowViewModel) {
+        
+        bind(viewModal: viewModal)
+      
+        guard viewModal.image?.value == nil else {
+            return
+        }
+        
+        viewModal.loadImage { (image, error) in
+            
+            guard let error = error else {
+                return
+            }
+            
+            print("error on fetch Image \(error.localizedDescription)")
+        }
+        
+    }
+    
+    private func bind(viewModal : ServerRowViewModel) {
+        
+        viewModal.title?.observe {[weak self] (newValue, _) in
+            self?.titleLabel.text = newValue
+        }
+        
+        viewModal.image?.observe({[weak self] (image, _) in
+            self?.rowImageView.image = image
+        }).add(to: &disposal)
+        
+        viewModal.description?.observe {[weak self] (newValue, _) in
+            self?.descriptionLabel.text = newValue
+        }
+    }
+
+}
